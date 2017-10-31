719e66f5983ae9665e520a7da53f3e0e21e83070
stefan <fustarstefan@gmail.com>
2017-10-31 14:49:45 +0100

    Lesson 3
    adding replies to threads as well as unit tests for it

--- app/Thread.php
+++ app/Thread.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    public function path(){
+    public function path()
+    {
         return '/threads/'. $this->id;
     }
+
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+
+
 }
