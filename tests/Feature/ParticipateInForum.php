4f43d4168602d3b896a8d235a3b8394a2df6ae57
stefan <fustarstefan@gmail.com>
2017-11-13 12:58:31 +0100

    prebacivanje na novi komp

---
+++ tests/Feature/ParticipateInForum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ParticipateInForum extends TestCase
+{
+    /** @test */
+    function an_authenticated_user_can_participate_in_forum_threads()
+    {
+
+    }
+}
