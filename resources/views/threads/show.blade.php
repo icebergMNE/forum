67b39ad45a765d792bac6227a27001ac2ffaa76b
stefan <fustarstefan@gmail.com>
2017-11-14 17:11:58 +0100

    Creating form to submit new replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -28,5 +28,27 @@
                 @endforeach
             </div>
         </div>
+
+        @auth
+            <div class="row" style="color: #333333">
+                <div class="col-md-8 col-md-offset-2">
+                    <form action="{{ $thread->path() . '/replies' }}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="have soething to say?" rows="5">
+                            </textarea>
+                            <button type="submit" class="btn btn-default">Post</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        @else
+            <div class="row" style="color: #333333;text-align: center">
+                <div class="col-md-8 col-md-offset-2">
+                    <p>please <a href="{{ route('login') }}">sing in</a> to participate in conversation!</p>
+                </div>
+            </div>
+        @endauth
+        <div class="row" style="padding-bottom: 200px;"></div>
     </div>
 @endsection
